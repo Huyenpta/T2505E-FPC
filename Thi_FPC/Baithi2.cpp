77ef8b9cc2490cdc006e3953694f292d26464ca1
Huyenpta <huyenpta1990@gmail.com>
2025-07-03 19:02:40 +0700

    Nộp bài thi môn FPC

--- Thi_FPC/Baithi2.cpp
+++ Thi_FPC/Baithi2.cpp
@@ -1,19 +1,9 @@
 #include <stdio.h>
+#include "hamisprime.h"
 
-int isPrime(int n) {
-    if (n < 2)
-        return 0;
-
-    for (int i = 2; i <= n/2; i++) {
-        if (n % i == 0)
-            return 0; 
-    }
-    return 1; 
-}
 
 int main() {
     int n;
-
     printf("Nhap mot so nguyen duong: ");
     scanf("%d", &n);
 
